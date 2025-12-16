2cdbb8a9bef0332a84f83b0a9b10c0652e75d84d
Joseph Lau <jlau.dev@gmail.com>
2025-12-16 23:15:49 +0800

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,27 +1,23 @@
 export default function Gallery() {
   return (
     <>
-      <section 
-        className="px-6 py-20 text-center min-h-screen flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat relative"
-        style={{ backgroundImage: 'url(/images/hero.jpg)' }} id="hero"
-      >
-        <div className="absolute inset-0 bg-black/30"></div>
-        
-        <div className="relative z-10">
-          <h2 className="text-7xl font-bold mb-4 text-white">
-            Gallery
-          </h2>
-          <p className="text-2xl mb-12 text-white">
-            Check out our past events
-          </p>
-        </div>
-      </section>
+      <div className="px-6 py-20 bg-[#FEEFEC] text-center text-[#82310e]">
+        <h1 className="text-5xl sm:text-6xl font-bold mb-10">
+          Gallery
+        </h1>
+        <h2 className="text-xl sm:text-2xl mb-8">
+          Welcome to our gallery!
+        </h2>
+        <p className="mb-5">
+          Take a deep dive into our collection of hens party's and pictures of our hunky models.
+        </p>
+        <p>
+          Follow us on Facebook or Instagram to keep up with all our fun and sexy art classes and check out our hot talent. 
+        </p>
+      </div>
 
       <section className="px-6 py-20 bg-white">
         <div className="max-w-6xl mx-auto">
-          <h2 className="text-4xl font-bold mb-12 text-center text-[#82310e]">
-            Hens Party Memories
-          </h2>
           
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {/* Gallery items */}
